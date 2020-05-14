b737b4b3d222ca681c22e5a12abefd2fcd7ba091
chaima <chaima.dey@esprit.tn>
2020-05-14 05:00:26 +0100

    chaima

--- app/src/main/java/com/example/bioregproject/DataBases/BioRegDB.java
+++ app/src/main/java/com/example/bioregproject/DataBases/BioRegDB.java
@@ -9,23 +9,39 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 import com.example.bioregproject.DAOs.AccountDAO;
+import com.example.bioregproject.DAOs.CategorieDao;
 import com.example.bioregproject.DAOs.CategoryDAO;
+import com.example.bioregproject.DAOs.FournisseurDAO;
 import com.example.bioregproject.DAOs.HistoryDAO;
 import com.example.bioregproject.DAOs.NotificationDAO;
+import com.example.bioregproject.DAOs.OilDAO;
 import com.example.bioregproject.DAOs.PersoTaskDAO;
+import com.example.bioregproject.DAOs.PostDAO;
 import com.example.bioregproject.DAOs.ProductDAO;
 import com.example.bioregproject.DAOs.ProductLogsDAO;
+import com.example.bioregproject.DAOs.ProduitDAO;
+import com.example.bioregproject.DAOs.StorageDAO;
+import com.example.bioregproject.DAOs.SurfaceDao;
+import com.example.bioregproject.DAOs.TacheDao;
 import com.example.bioregproject.entities.Account;
+import com.example.bioregproject.entities.CategorieTache;
 import com.example.bioregproject.entities.Category;
+import com.example.bioregproject.entities.Fournisseur;
 import com.example.bioregproject.entities.History;
 import com.example.bioregproject.entities.Notification;
+import com.example.bioregproject.entities.Oil;
 import com.example.bioregproject.entities.PersoTask;
+import com.example.bioregproject.entities.Post;
 import com.example.bioregproject.entities.ProductLogs;
 import com.example.bioregproject.entities.Products;
+import com.example.bioregproject.entities.Produit;
+import com.example.bioregproject.entities.Storage;
+import com.example.bioregproject.entities.Surface;
+import com.example.bioregproject.entities.Tache;
 
 import java.util.Date;
 
-@Database(entities = {Account.class, Category.class, ProductLogs.class, Products.class, Notification.class, History.class, PersoTask.class},version = 8)
+@Database(entities = {Account.class, Category.class, ProductLogs.class, Products.class, Notification.class, History.class, PersoTask.class, Oil.class, Post.class, Tache.class, Surface.class, CategorieTache.class, Fournisseur.class, Produit.class, Storage.class},version = 16)
 
 public abstract class BioRegDB  extends RoomDatabase {
 
@@ -37,6 +53,15 @@ public abstract class BioRegDB  extends RoomDatabase {
     public abstract NotificationDAO notificationDAO();
     public abstract HistoryDAO historyDAO();
     public abstract PersoTaskDAO persoTaskDAO();
+    public abstract OilDAO oilDAO();
+    public abstract PostDAO postDAO();
+    public abstract SurfaceDao surfaceDao();
+    public abstract CategorieDao categorieCleanDao();
+    public abstract TacheDao cleanPlanificationDao();
+    public abstract FournisseurDAO FournisseurDAO();
+    public abstract ProduitDAO ProduitDAO();
+    public abstract StorageDAO StorageDAO();
+
 
     public static synchronized BioRegDB getInstance(Context context){
         if(instance == null)
